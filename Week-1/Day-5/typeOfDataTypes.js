26a95684ad6a0932af98cd87e78f749e45b3ecfa
Sukriti Mishra <sukritippl@gmail.com>
2020-07-25 21:43:38 +0530

    Added  all the files to complete Day-5 tasks

---
+++ Week-1/Day-5/typeOfDataTypes.js
@@ -0,0 +1,17 @@
+console.log("Type of 1.1 is "+ typeof 1.1);
+
+console.log("Type of 1 is "+ typeof 1);
+
+console.log("Type of true is "+ typeof true);
+console.log("Type of null is "+ typeof null);
+console.log("Type of undefined is "+ typeof undefined);
+console.log("Type of [] is "+ typeof []);
+console.log("Type of {} is "+ typeof {});
+console.log("type of char a is " + typeof 'a' );
+console.log("type of string abc is " + typeof "abc" );
+let x= function fun(){
+	console.log("Type of fun function is "+ typeof fun);
+}
+
+console.log("type of variable x is " + typeof x );
+
